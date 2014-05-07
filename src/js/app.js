ce1a8d332cccd10e9b262952a0b74fca10bfe578
lucas <lucasjonesdev@hotmail.co.uk>
2014-05-07 16:33:46 +0100

    Save configuration to file

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 var gui = require('nw.gui');
+var fs = require('fs');
+var async = require('async');
+var mkdirp = require('mkdirp');
+
 var mainWindow = gui.Window.get();
 
 function openDialog(url, options) {
@@ -118,35 +122,102 @@ nompApp.config(function ($routeProvider) {
 });
 
 nompApp.run(function ($rootScope) {
-    initGUI($rootScope);
+    async.series([
+        function (callback) {
+            $rootScope.dataPath = gui.App.dataPath;
+            $rootScope.configFilePath = $rootScope.dataPath + '/config.json';
+            $rootScope.writeConfigFile = function (callback) {
+                var config = {};
+                config.pools = $rootScope.pools;
+                config.keys = $rootScope.keys;
+                config.nextPoolId = 0;
+                fs.writeFile($rootScope.configFilePath, angular.toJson(config, true), function (err) {
+                    if (err) {
+                        if (callback) {
+                            callback("Failed to write to config file(" + $rootScope.configFilePath + "): " + err);
+                        } else {
+                            throw "Failed to write to config file(" + $rootScope.configFilePath + "): " + err;
+                        }
+                    }
+                    if (callback) {
+                        callback(null);
+                    }
+                });
+            };
 
-    // The user's saved pools
-    $rootScope.pools = [
-        {id: 1, name: 'NompPool', url: 'example.com', users: ['ca23629e2647387e99542fc0b25cdf75e101c3c0']},
-        {id: 2, name: 'TestPool', url: '127.0.0.1:2000', users: ['1KRotMnQpxu3sePQnsVLRy3EraRFYfJQFR']}
-    ];
-    $rootScope.nextPoolId = 5;
-    // The user's mining keys
-    $rootScope.keys = [
-        {id: 1, name: 'Mining wallet 1', hash160: 'ca23629e2647387e99542fc0b25cdf75e101c3c0', watch_only: true}
-    ];
-    $rootScope.showAddPoolDialog = function () {
-        openDialog('app://local/html/dialogs/add_pool.html', {width: 400, height: 250})
-            .on('addPool', function (url, user) {
-                $rootScope.$apply(function () {
-                    $rootScope.pools.push({
-                        id: $rootScope.nextPoolId++,
-                        name: 'Unknown',
-                        url: url,
-                        user: user
+            // The user's saved pools
+            $rootScope.pools = [
+                {id: 1, name: 'NompPool', url: 'example.com', users: ['ca23629e2647387e99542fc0b25cdf75e101c3c0']},
+                {id: 2, name: 'TestPool', url: '127.0.0.1:2000', users: ['1KRotMnQpxu3sePQnsVLRy3EraRFYfJQFR']}
+            ];
+            // ID to use for next pool
+            $rootScope.nextPoolId = 1;
+            $rootScope.$watchCollection('pools', function () {
+                for (var i = 0; i < $rootScope.pools.length; i++) {
+                    if ($rootScope.nextPoolId <= $rootScope.pools[i].id) {
+                        $rootScope.nextPoolId = $rootScope.pools[i].id + 1;
+                    }
+                }
+            });
+            // The user's mining keys
+            $rootScope.keys = [
+                {id: 1, name: 'Mining wallet 1', hash160: 'ca23629e2647387e99542fc0b25cdf75e101c3c0', watch_only: true}
+            ];
+            mkdirp($rootScope.dataPath, function (err) {
+                if (err) {
+                    throw "Failed to open data directory (" + $rootScope.dataPath + "): " + err;
+                }
+                fs.readFile($rootScope.configFilePath, function (err, data) {
+                    if ((err && err.code === 'ENOENT') || data == undefined) {
+                        $rootScope.writeConfigFile(function (err) {
+                            if (err) throw err;
+                            callback(null);
+                        });
+                        return;
+                    }
+                    if (err) {
+                        throw "Failed to open config file(" + $rootScope.configFilePath + "): " + err.code;
+                    }
+                    try {
+                        data = JSON.parse(data);
+                    } catch (err) {
+                        throw "Failed to read config file" + $rootScope.configFilePath + "): " + err;
+                    }
+                    $rootScope.$apply(function () {
+                        $rootScope.pools = data.pools || [];
+                        $rootScope.keys = data.keys || [];
+                        callback(null);
                     });
                 });
             });
-    };
-    $rootScope.showNewKeyDialog = function () {
-        openDialog('app://local/html/dialogs/new_key.html', {width: 600, height: 300});
-    };
-    $rootScope.showOptionsDialog = function () {
-        openDialog('app://local/html/dialogs/options.html', {width: 600, height: 400});
-    };
+        },
+        function (callback) {
+            $rootScope.$watchCollection('pools', function () {
+                $rootScope.writeConfigFile();
+            });
+            $rootScope.$watchCollection('keys', function () {
+                $rootScope.writeConfigFile();
+            });
+            initGUI($rootScope);
+            $rootScope.showAddPoolDialog = function () {
+                openDialog('app://local/html/dialogs/add_pool.html', {width: 400, height: 250})
+                    .on('addPool', function (url, user) {
+                        $rootScope.$apply(function () {
+                            $rootScope.pools.push({
+                                id: $rootScope.nextPoolId++,
+                                url: url,
+                                user: user
+                            });
+                        });
+                    });
+            };
+            $rootScope.showNewKeyDialog = function () {
+                openDialog('app://local/html/dialogs/new_key.html', {width: 600, height: 300});
+            };
+            $rootScope.showOptionsDialog = function () {
+                openDialog('app://local/html/dialogs/options.html', {width: 600, height: 400});
+            };
+            callback(null);
+        }
+    ]);
 });
